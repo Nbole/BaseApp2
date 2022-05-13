baa6ca1967448e6c71fb85ae8b4abdd8f574813f
Nicolas <nicolas.bole@hotmail.com>
2022-05-13 11:08:37 -0300

    wip

--- app/src/main/java/com/example/baseapp/data/remote/MovieDataApi.kt
+++ app/src/main/java/com/example/baseapp/data/remote/MovieDataApi.kt
@@ -4,11 +4,9 @@ import com.example.baseapp.BuildConfig
 import com.example.baseapp.data.remote.di.KtorClient
 import com.example.baseapp.data.remote.model.MovieResult
 import com.example.baseapp.domain.MovieDataContract
-import io.ktor.client.features.ClientRequestException
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.url
-import java.io.IOException
 
 class MovieDataApi : MovieDataContract {
     override suspend fun getLatestMovies(): Response<MovieResult> =
@@ -16,14 +14,4 @@ class MovieDataApi : MovieDataContract {
             url(BuildConfig.BASE_URL + "now_playing")
             parameter("api_key", BuildConfig.API_KEY)
         }.toResponse()
-}
-
-fun <T> T.toResponse(): Response<T> {
-    return try {
-        Response.Success(this)
-    } catch (e: ClientRequestException) {
-        Response.Error(data = null, message = e.message)
-    } catch (e: IOException) {
-        Response.Error(data = null, message = e.message.orEmpty())
-    }
 }
--- app/src/main/java/com/example/baseapp/data/remote/Response.kt
+++ app/src/main/java/com/example/baseapp/data/remote/Response.kt
@@ -1,5 +1,8 @@
 package com.example.baseapp.data.remote
 
+import io.ktor.client.features.ClientRequestException
+import java.io.IOException
+
 sealed class Response<T>(
     val data: T? = null,
     val message: String? = null
@@ -7,4 +10,14 @@ sealed class Response<T>(
     class Success<T>(data: T) : Response<T>(data)
     class Loading<T>(data: T? = null) : Response<T>(data)
     class Error<T>(message: String, data: T? = null) : Response<T>(data, message)
+}
+
+fun <T> T.toResponse(): Response<T> {
+    return try {
+        Response.Success(this)
+    } catch (e: ClientRequestException) {
+        Response.Error(data = null, message = e.message)
+    } catch (e: IOException) {
+        Response.Error(data = null, message = e.message.orEmpty())
+    }
 }
